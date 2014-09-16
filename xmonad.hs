81429caf8a1aae15010e745285a08a056f8f59e0
Christopher <ggmethos@autistici.org>
2014-09-16 01:13:09 -0400

    remove kebab

--- xmonad.hs
+++ xmonad.hs
@@ -2,9 +2,6 @@ import System.Posix.Env (getEnv)
 import Data.Maybe (maybe)
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Config.Gnome
-import XMonad.Config.Kde
-import XMonad.Config.Xfce
 import XMonad.Layout.NoBorders
 
 normalBorderColor = "#000000"
@@ -16,9 +13,6 @@ main = xmonad defaultConfig
         {   terminal = "terminator",
             layoutHook = myLayoutHook
         }
-
-desktop "gnome" = gnomeConfig
-desktop "kde" = kde4Config
-desktop "xfce" = xfceConfig  
+        
 desktop "xmonad-mate" = gnomeConfig
 desktop _ = desktopConfig
