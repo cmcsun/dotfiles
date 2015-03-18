008cb9e2bf83f3e2b428bea912926942cd804de2
Methos <ggmethos@autistici.org>
2015-03-18 15:16:07 -0400

    few fixes

---
+++ .emacs.d/elpa/sicp-20121014.1314/sicp-autoloads.el
@@ -0,0 +1,15 @@
+;;; sicp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("sicp-pkg.el") (21769 50215 762926 689000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sicp-autoloads.el ends here
