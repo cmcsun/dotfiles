5179fc5be7028e83392fe22e034ed21558b8ff99
Methos <ggmethos@autistici.org>
2015-08-22 04:25:51 -0400

    borders suck

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,13 +4,13 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Layout.NoBorders
 
-normalBorderColor = "#000000"
-focusedBorderColor = "#000000"
-
+-- |normalBorderColor = "#000000"
 myLayoutHook = noBorders Full
-
 main = xmonad defaultConfig
     {
+        normalBorderColor = "#000000",
+        focusedBorderColor = "#000000",
+        borderWidth = 0,
         terminal = "sakura",
         layoutHook = myLayoutHook
     }
